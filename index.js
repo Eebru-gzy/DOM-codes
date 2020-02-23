2f892b23317e4afa09274675f240b04a0811dbc7
Ibrahim <Alaoopeyemi101@gmail.com>
2020-02-23 15:03:02 +0100

    multiple selector

--- index.js
+++ index.js
@@ -54,50 +54,52 @@ console.log(val);
 // 1) document.getElementById( )
 // 2) document.querySelector(): when using it to select tag name, it would jsut selct only the first one
 
-document.querySelector('#task-title'); //selects the element id
-document.querySelector('.task-title'); //selects the element class
-document.querySelector('h5'); //selects the first h5
+// document.querySelector('#task-title'); //selects the element id
+// document.querySelector('.task-title'); //selects the element class
+// document.querySelector('h5'); //selects the first h5
 
-//styling
-document.querySelector('li').style.color = 'green'; 
-document.querySelector('ul li').style.color = 'blue'; 
+// //styling
+// document.querySelector('li').style.color = 'green'; 
+// document.querySelector('ul li').style.color = 'blue'; 
 
-document.querySelector("li:last-child").style.color = "blue"; 
-document.querySelector("li:nth-child(4)").style.color = "blue"; 
+// document.querySelector("li:last-child").style.color = "blue"; 
+// document.querySelector("li:nth-child(4)").style.color = "blue"; 
 
-// because querySelector is a single element selector, if we use even and odd on the nth-child, it would only select the first
+// // because querySelector is a single element selector, if we use even and odd on the nth-child, it would only select the first
 
-document.querySelector("li:nth-child(even)").style.background = "blue"; 
+// document.querySelector("li:nth-child(even)").style.background = "blue"; 
 
 
 //////Mulltiple Selectors: getElementsByClassName
+//on Global scale
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.background = 'yellow';
+// items[0].textContent = 'Eebru';
+ 
+// //this is more particular to the class names inside of the ul
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
 
-const list = document.getElementsByClassName('collection-item');
-console.log(list);
+// // getElementByTagName:
+// document.getElementsByTagName('li');
 
+
+
+//querySelectorAll: returns a nodeList
+
+// document.querySelectorAll('ul.collection li.collection-item'); //it accepts typical css selection, somearray methods can be used on it, but it's stilla not an array
+
+//   const liEven = document.querySelectorAll('li:nth-child(even)');
+//   const liOdd = document.querySelectorAll('li:nth-child(odd)');
+
+// liEven.forEach(cur => {
+//     cur.style.background = '#f4f4f4'
+// })
+
+// for (let i = 0; i < liOdd.length; i++) liOdd[i].style.background = '#ccc'
 
 
  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
